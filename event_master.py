207b824e5c49e04244dc275f55a07d0fb7ca923d
Bob Hinkle <ex@hp-debian>
2016-09-23 00:08:21 -0400

    Changed log() to automatically add timestamping; DRY! Altered start() to minimize complexity. Functions call next in order.

--- event_master.py
+++ event_master.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#call script with 2 arguments: <serialnumber> <accesscode>
+# call script with 2 arguments: <serialnumber> <accesscode>
 
 import serial
 import os
@@ -9,111 +9,89 @@ import sys
 from datetime import datetime
 
 
-
-#globals
+# globals
 ser = serial.Serial()
-maxtimeouts = 15            #number of times to try getting a response from seismo
-interval = 1800               #number of seconds to wait between data checks/dumps
-running = 0                 #is the seismo in scan mode? 0 = no, 1 = yes
-directory = "/home/pi/FTP/"  #directory to download events to
-logfile = "log.txt"         #name of log file
-
-
+maxtimeouts = 15  # number of times to try getting a response from seismo
+interval = 1800  # number of seconds to wait between data checks/dumps
+running = 0  # is the seismo in scan mode? 0 = no, 1 = yes
+directory = "/home/pi/FTP/"  # directory to download events to
+logfile = "log.txt"  # name of log file
 
 
 try:
     serialnum = str(sys.argv[1])
     accesscode = str(sys.argv[2])
     logincmd = "key " + accesscode + "\r"
-    
-    
+
+
 except:
-    raise Exception('Please call on script in the following manner: python <script> <serial> <ac>')
-
-
-
-
-
+    raise Exception(
+        'Please call on script in the following manner: python <script> <serial> <ac>')
 
 
 def timestamp():
     return datetime.strftime(datetime.now(), '%m-%d-%Y %H:%M:%S -')
 
 
-
-
 def stopped():
-    log(timestamp() + " - Seismo stopped commicating for some reason.")
+    log("Seismo stopped commicating for some reason.")
 
-
-
-
-    
 
 def toggleDTR():
-    #toggle DTR to 'wake-up' 3000 machine's serial port comms
+    # toggle DTR to 'wake-up' 3000 machine's serial port comms
     ser.setDTR(0)
     time.sleep(2)
     ser.setDTR(1)
     time.sleep(3)
 
 
-
-
-
 def wakeup():
-    #toggle DTR and listen for 'ATE0'   exit on successfully finding 'ATE0'
+    # toggle DTR and listen for 'ATE0'   exit on successfully finding 'ATE0'
     ser.close()
     ser.open()
     a = ser.read(ser.inWaiting())
-    log(timestamp() + " - Waiting for seismo to acknowledge...")
+    log("Waiting for seismo to acknowledge...")
     timeout = 0
     while(a.find('ATE0') == -1):
         if(timeout > maxtimeouts):
-            log(timestamp() + " - Seismo not responding. It could be busy, bad battery, or bad comm setup.")
-            return 0
+            log(timestamp(
+            ) + " - Seismo not responding. It could be busy, bad battery, or bad comm setup.")
+            end()
         else:
             timeout += 1
             toggleDTR()
             a = ser.read(ser.inWaiting())
-    log(timestamp() +  " - OK.")
-    return 1
+    log("OK.")
+    check()
 
 
-
-
-
-    
 def init():
-    #setup com port
-    
-    #some simple error handling here
+    # setup com port
+
+    # some simple error handling here
     try:
         ser.port = "/dev/ttyUSB0"
     except:
         raise Exception('Could not use ttyUSB0 for comms')
-        
+
     ser.baudrate = 9600
 
-    #Check if there is a log file already...
+    # Check if there is a log file already...
     #  if true, it's all good... our log() will work
     #  if false, create one!
-    if(os.path.isfile(directory+logfile)):
+    if(os.path.isfile(directory + logfile)):
         print("Logfile present.")
     else:
-        f = open(directory+logfile, 'w')
+        f = open(directory + logfile, 'w')
         f.close()
-    log(timestamp() + " - Starting script--------------------------------------------------")
-        
-    
+    log("Starting script--------------------------------------------------")
 
 
 def log(string):
-    f = open(directory+logfile, 'a')
-    f.write(string+'\n')
+    f = open(directory + logfile, 'a')
+    f.write(timestamp() + " - " + string + '\n')
     f.close()
     print(string)
-
 
 
 def prompt():
@@ -122,66 +100,63 @@ def prompt():
     timeout = 0
     while(a.find('>') == -1):
         if(timeout > maxtimeouts):
+            log("Trouble commicating with siesmo.")
             return 0
         else:
             timeout += 1
-            log(timestamp() + " -  Trying to get a prompt " + str(timeout) + " out of "
-                + str(maxtimeouts))
+            log(" Trying to get a prompt " + str(timeout) +
+                " out of " + str(maxtimeouts))
             time.sleep(10)
             ser.write('\r')
             a = ser.read(ser.inWaiting())
     return 1
 
 
-
-
-
 def check():
-    log(timestamp() + " - Checking seismo for new data...")
+    log("Checking seismo for new data...")
     ser.write('inf\r')
     time.sleep(2)
     a = ser.read(ser.inWaiting())
     if(a.find('[05.') != -1):
-        #split returned string to find out how many events are on seismo
+        # split returned string to find out how many events are on seismo
         b = a.split("(")
         c = b[1].split(",")
-        log(timestamp() + " - " + c[0] + " values found.")
+        log(c[0] + " values found.")
         if(int(c[0]) > 1):
-            return int(c[0])          #return how many
+            if(prompt()):
+                login()
+            else:
+                stopped()
+                start()
         else:
-            log(timestamp() + " -  Just a template.")
-            return 1                #return just 1, check was successful but only the template is on seismo
-    return 0                        #lost comms?
-
-
-
-
+            log(" Just a template.")
+            end()
+    else:
+        log("Incorrect firmware?")
+    end()
 
 
 def login():
-    log(timestamp() + " - Logging into seismo...")
+    log("Logging into seismo...")
     ser.write(logincmd)
     time.sleep(2)
     a = ser.read(ser.inWaiting())
     if(a.find('OK') != -1):
-        log(timestamp() + " - OK.")
-        return 1
+        log("OK.")
+        is_running()
     else:
-        return 0
-
-
-        
-
+        log("Incorrect response from seismo. Restarting comms.")
+        start()
 
 
 def is_running():
     global running
-    log(timestamp() + " - Checking to see if seismo is currently scanning...")
+    log("Checking to see if seismo is currently scanning...")
     ser.write("lst\r")
     time.sleep(5)
     a = ser.read(ser.inWaiting())
     if(a.find('NOT STOPPED') != -1):
-        log(timestamp() + " - Currently in scan mode. Halting...")
+        log("Currently in scan mode. Halting...")
         running = 1
         b = prompt()
         if(b):
@@ -189,29 +164,25 @@ def is_running():
             time.sleep(2)
             c = ser.read(ser.inWaiting())
             if(c.find('OK') != -1):
-                log(timestamp() + " - Temporarily halted.")
-                return 1
+                log("Temporarily halted.")
+                dump()
         else:
             stopped()
-            return 0
+            start()
     else:
         running = 0
-        log(timestamp() + " - Not in scan mode.")
-        return 1
-
-              
-
-
+        log("Not in scan mode.")
+        dump()
 
 
 def dump():
-    log(timestamp() + " - Transferring files...")
+    log("Transferring files...")
     try:
         os.system('sudo stty -F /dev/ttyUSB0 9600')
         os.chdir(directory)
     except:
         raise Exception('Could not connect ttyUSB0 to terminal')
-        
+
     ser.write('xfr 0\r')
 
     try:
@@ -219,75 +190,54 @@ def dump():
     except:
         raise Exception('Problem recieving files with rb --ymodem')
     time.sleep(5)
-    log(timestamp() + " - Transfer complete!")
+    log("Transfer complete!")
     a = prompt()
     if(a):
-        log(timestamp() + " - Clearing events downloaded from seismo..")
+        log("Clearing events downloaded from seismo..")
         ser.write("clr E\r")
         time.sleep(2)
         b = ser.read(ser.inWaiting())
         if (b.find('OK') != -1):
-            log(timestamp() + " - Events cleared.")
+            log("Events cleared.")
     else:
         stopped()
-        
-
-
+    run()
 
 
 def run():
     if(running == 1):
-        log(timestamp() + " - Returning unit back to scan mode...")
+        log("Returning unit back to scan mode...")
         ser.write("run\r\r")
         time.sleep(5)
         b = ser.read(ser.inWaiting())
         if(b.find('OK') == -1):
             stopped()
-            return 0
+            run()
         c = prompt()
         if(c == 0):
             stopped()
-            return 0
-        log(timestamp() + " - Unit scanning.")
-    return 1
-
-
-
+            end()
+        log("Unit scanning.")
+        end()
 
 
 def end():
-    log(timestamp() + " - Ending communications with seismo.")
-    
-
-            
+    log("Ending communications with seismo.")
 
 
+# wakeup -> check -> prompt -> login -> is_running -> dump -> run -> wait -> wakeup
+#
+#
+#
+
+# main program loop
 def start():
     while(1):
-        a = wakeup()
-        if(a):
-            b = check()
-            if(b > 1):
-                c = prompt()
-                if(c):
-                    d = login()
-                    if(d):
-                        e = is_running()
-                        if(e):
-                            dump()
-                            run()
-                        else:
-                            start()
-                    else:
-                        start()
-                else:
-                    start()
-        end()
+        wakeup()
         log("Waiting " + str(interval) + " seconds before next check.")
         time.sleep(interval)
 
-                  
-print timestamp(), "Script starting..."            
+
+print timestamp(), "Script starting..."
 init()
 start()
-
